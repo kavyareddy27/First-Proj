67e7e8f1d1cc030586740760fb5e1b5b730fd4b3
kavyareddy27 <kavchinn@umail.iu.edu>
2016-07-14 20:17:47 -0400

    Check myprofile2.html for info of states and calculator.html

---
+++ js/testscript.js
@@ -0,0 +1,26 @@
+function showImg1(){
+$(".details").hide();
+$("#indy").show();
+};
+function showImg2(){
+$(".details").hide();
+$("#mi").show();
+};
+function showImg3(){
+$(".details").hide();
+$("#chi").show();
+};
+function showImg4(){
+$(".details").hide();
+$("#oh").show();
+};
+$(document).ready(function(){
+$(".details").hide();
+$("#indy").show();
+});
+var test={
+	indy:{
+		p:"Indiana is a Midwestern state known for its farmland and renowned auto race, the Indianapolis 500. In the capital, Indianapolis, theaters and galleries line Massachusetts Avenue, while Broad Ripple has hip bars and restaurants. The city’s Downtown is home to the iconic Soldiers and Sailors Monument, the Canal Walk promenade and the Indianapolis Museum of Art and its wide-ranging collections.";
+		img src:"../images/michigan.gif" style="height:200px";
+	}
+}
